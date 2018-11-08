d788eefa6066ded15e7934cd2fadb25b2f44cf67
zhengweilin <190215076@qq.com>
2018-11-08 18:14:29 +0800

    init

--- src/router.ts
+++ src/router.ts
@@ -5,12 +5,13 @@ import Home from './views/Home.vue'
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
       name: 'home',
       component: Home,
-    },
+    }, 
     {
       path: '/about',
       name: 'about',
